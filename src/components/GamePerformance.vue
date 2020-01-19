940a7e7106c1e4c76dcc96c2a589f13f06e0c173
EskoJuhani <eskojuhani@gmail.com>
2020-01-19 09:12:35 +0200

    dev

---
+++ src/components/GamePerformance.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="performance" class="entity-list">
+    <div v-for="(item, index) in performance" v-bind:key="index" class="entity-box">
+      <div>{{ item.gameDate }}</div>
+      <div>{{ item.location }}</div>
+      <div>{{ item.performance }}</div>
+      <div>{{ item.gameMovingAvg3 }}</div>
+      <div>{{ item.gameMovingAvg10 }}</div>
+      <div>{{ item.outcome }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GamePerformance',
+  props: {
+    performance: { type: Array, required: false }
+  }
+}
+</script>
